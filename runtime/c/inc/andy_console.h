013afa7c8c868f9b0fd8aa2f29b26fa0536cbddf
asuttles <andrew.suttles@gmail.com>
2026-03-14 19:19:23 -0400

    Added console text functions.
    - Added functions for colors, bold, italic, underline, etc.
    - Updated example andy programs to demonstrate these capabilities.
    - Updated builtin lisp datastructure to know about c built in functions.

--- runtime/c/inc/andy_console.h
+++ runtime/c/inc/andy_console.h
@@ -6,5 +6,14 @@
 #include "andy_types.h"
 
 void andy_clearScreen(void);
+void andy_resetConsole(void);
+void andy_clearLine(void);
+void andy_bold(void);
+void andy_italic(void);
+void andy_underline(void);
+void andy_blink(void);
+void andy_reverse(void);
+void andy_setForegroundRGB(andy_int r, andy_int g, andy_int b);
+void andy_setBackgroundRGB(andy_int r, andy_int g, andy_int b);
 
 #endif
--- runtime/c/src/andy_console.c
+++ runtime/c/src/andy_console.c
@@ -4,3 +4,49 @@ void andy_clearScreen(void) {
 
   puts("\033[2J\033[H");
 }
+
+void andy_resetConsole(void) {
+
+  puts("\x1b[0m");
+}
+
+void andy_clearLine(void) {
+
+  puts("\x1b[2K");
+}
+
+void andy_bold(void) {
+
+  puts("\x1b[1m");
+}
+
+void andy_italic(void) {
+
+  puts("\x1b[3m");
+}
+
+void andy_underline(void) {
+
+  puts("\x1b[4m");
+}
+
+void andy_blink(void) {
+
+  puts("\x1b[5m");
+}
+
+void andy_reverse(void) {
+
+  puts("\x1b[7m");
+}
+
+void andy_setForegroundRGB(andy_int r, andy_int g, andy_int b) {
+
+  printf("\x1b[38;2;%d;%d;%dm", (int)r, (int)g, (int)b);
+}
+
+void andy_setBackgroundRGB(andy_int r, andy_int g, andy_int b) {
+
+  printf("\x1b[48;2;%d;%d;%dm", (int)r, (int)g, (int)b);
+}
+
